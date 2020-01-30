fd14c3923c20a13e15c496ce9f8e31a562ace5ed
Raul302 <Luis_1547@hotmail.com>
2020-01-29 21:11:55 -0600

    personasController

--- app/controllers/personas_controller.rb
+++ app/controllers/personas_controller.rb
@@ -4,4 +4,21 @@ class PersonasController < ApplicationController
         @personas = Persona.all
     end
 
+    def new 
+        @personas = Persona.new
+    end
+    
+    def create
+        @personas = Persona.find(persona_params)
+          if @personas.save
+            redirect_to(personas_path)
+          else
+            render :new
+          end
+    end
+
+    private
+      def persona_params
+        params.require(:personas).permit(:name, :apellido, :sexo, :edad)
+      end
 end
